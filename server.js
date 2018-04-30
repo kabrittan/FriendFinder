abad7f73434470c8f4855c813b518b7042a70958
Kyle Brittan <kabrittan@gmail.com>
2018-04-29 22:14:34 -0700

    Debugged Routing

--- server.js
+++ server.js
@@ -9,12 +9,11 @@ var PORT = process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-//Routes
+//Require Routes
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-//
+//Server Listening
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
-  
