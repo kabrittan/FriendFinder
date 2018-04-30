0791919cec4fc126529430572027b7b00c36fb8a
Kyle Brittan <kabrittan@gmail.com>
2018-04-29 19:36:55 -0700

    Adder Server JS

---
+++ server.js
@@ -0,0 +1,20 @@
+//Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+
+//Create Express App
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+//Routes
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+//
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
