ff560cc530f8a6487f122f485f79818d7da2963a
Kyle Brittan <kabrittan@gmail.com>
2018-04-29 17:26:59 -0700

    Added API Routing

---
+++ app/data/friends.js
@@ -0,0 +1,9 @@
+var friendArr = [
+    {
+        name: "Ky Guy",
+        image: "https://marriedwiki.com/uploads/bio/sam-elliott.jpg";
+        answers: [2, 3, 1, 5, 4, 1, 5, 3, 2, 3]
+    }
+];
+
+module.exports = friendArr;
