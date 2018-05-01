2b2a16951695dd3441a6cc18d79451485ea8fed2
Kyle Brittan <kabrittan@gmail.com>
2018-04-30 18:15:03 -0700

    Debugged Routing API

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ var friendsData = require("../data/friends");
 
 module.exports = function(app) {
     app.get("/api/friends", function (req, res) {
-        res.json(friendsData);
+        res.JSON(friendsData);
     });
 
     app.post("/api/friends", function (req, res) {
